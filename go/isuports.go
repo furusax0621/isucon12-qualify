527b29f19a55efdb791c0fa4ebb9e0ea652011ed
Takaki Kobayashi <takaki.kobayashi@fuller.co.jp>
2022-07-23 15:45:53 +0900

    flockByTenantID をつかわない

--- go/isuports.go
+++ go/isuports.go
@@ -639,11 +639,17 @@ func billingReportByCompetition(ctx context.Context, tenantDB dbOrTx, tenantID i
 	}
 
 	// player_scoreを読んでいるときに更新が走ると不整合が起こるのでロックを取得する
-	fl, err := flockByTenantID(tenantID)
-	if err != nil {
-		return nil, fmt.Errorf("error flockByTenantID: %w", err)
+	mu, ok := tenantDBLocks[tenantID]
+	if !ok {
+		return nil, fmt.Errorf("error tenantDB not found: %d", tenantID)
 	}
-	defer fl.Close()
+	mu.Lock()
+	defer mu.Unlock()
+	// fl, err := flockByTenantID(tenantID)
+	// if err != nil {
+	// 	return nil, fmt.Errorf("error flockByTenantID: %w", err)
+	// }
+	// defer fl.Close()
 
 	// スコアを登録した参加者のIDを取得する
 	scoredPlayerIDs := []string{}
@@ -1114,11 +1120,17 @@ func competitionScoreHandler(c echo.Context) error {
 	}
 
 	// / DELETEしたタイミングで参照が来ると空っぽのランキングになるのでロックする
-	fl, err := flockByTenantID(v.tenantID)
-	if err != nil {
-		return fmt.Errorf("error flockByTenantID: %w", err)
+	mu, ok := tenantDBLocks[v.tenantID]
+	if !ok {
+		return fmt.Errorf("error tenantDB not found: %d", v.tenantID)
 	}
-	defer fl.Close()
+	mu.Lock()
+	defer mu.Unlock()
+	// fl, err := flockByTenantID(v.tenantID)
+	// if err != nil {
+	// 	return fmt.Errorf("error flockByTenantID: %w", err)
+	// }
+	// defer fl.Close()
 	var rowNum int64
 	playerScoreRows := []PlayerScoreRow{}
 	for {
@@ -1312,11 +1324,17 @@ func playerHandler(c echo.Context) error {
 		return fmt.Errorf("error Select competition: %w", err)
 	}
 	// player_scoreを読んでいるときに更新が走ると不整合が起こるのでロックを取得する
-	fl, err := flockByTenantID(v.tenantID)
-	if err != nil {
-		return fmt.Errorf("error flockByTenantID: %w", err)
+	mu, ok := tenantDBLocks[v.tenantID]
+	if !ok {
+		return fmt.Errorf("error tenantDB not found: %d", v.tenantID)
 	}
-	defer fl.Close()
+	mu.Lock()
+	defer mu.Unlock()
+	// fl, err := flockByTenantID(v.tenantID)
+	// if err != nil {
+	// 	return fmt.Errorf("error flockByTenantID: %w", err)
+	// }
+	// defer fl.Close()
 	pss := make([]PlayerScoreRow, 0, len(cs))
 	for _, c := range cs {
 		ps := PlayerScoreRow{}
